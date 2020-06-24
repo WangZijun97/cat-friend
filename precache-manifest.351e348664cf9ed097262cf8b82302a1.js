acdb2ba7822f8f742c27719c14dbf8025260d0ff
Wang Zijun <wzj97@hotmail.com>
2020-06-24 17:34:53 +0800

    Updates

---
+++ precache-manifest.351e348664cf9ed097262cf8b82302a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a782373644cc51cbbc6ceea980ad6f6",
+    "url": "/cat-friend/index.html"
+  },
+  {
+    "revision": "e41e9562b5379b81e577",
+    "url": "/cat-friend/static/css/main.fb2e77fd.chunk.css"
+  },
+  {
+    "revision": "cefe60587215617ef565",
+    "url": "/cat-friend/static/js/2.b0d01d80.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-friend/static/js/2.b0d01d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e41e9562b5379b81e577",
+    "url": "/cat-friend/static/js/main.3720e878.chunk.js"
+  },
+  {
+    "revision": "b76c2dcfb94de671a620",
+    "url": "/cat-friend/static/js/runtime-main.7d078474.js"
+  }
+]);
